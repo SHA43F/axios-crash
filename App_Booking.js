7c8ab084e96a7c1c7c48773f84710a3dabc2cdb1
SHA43F <shareefpmk44@gmail.com>
2022-10-05 21:54:34 +0530

    DOM App_Booking

--- App_Booking.js
+++ App_Booking.js
@@ -13,46 +13,32 @@ function submitNodes(e)
 {
     e.preventDefault();
     let obj = {name: inpName.value, value: inpValue.value}
-    let obj_serial = JSON.stringify(obj)
     axios.post("https://crudcrud.com/api/24e5019e705344efa3e264d37ccd0792/objects", obj)
     .then((response) => {
-        console.log(response)})
+        addDataInto(response)})
     .catch((err) => console.log(err))
+    
+}
+
+function addDataInto(obj)
+{
     let li_items = document.getElementById('lists');
-    let lists = `<li id=${inpName.value}>${inpName.value} ${inpValue.value}
-                <button onClick=deleteVal('${inpName.value}${inpValue.value}','${inpName.value}')>Delete</button> 
-                <button onClick=editVal('${inpName.value}${inpValue.value}','${inpName.value}','${inpValue.value}')>Edit</button></li>`;
+    let lists = `<li id=${obj.name}>${obj.name} ${obj.value}
+                <button onClick=deleteVal('${obj.name}${obj.value}','${obj.name}')>Delete</button> 
+                <button onClick=editVal('${obj.name}${obj.value}','${obj.name}','${obj.value}')>Edit</button></li>`;
     li_items.innerHTML += lists;
     inpName.value = ""
     inpValue.value = ""
 }
-// Object.keys(localStorage).forEach((key) => 
-// {
-//     let keyes = localStorage.getItem(key);
-//     let vals = JSON.parse(keyes);
-//     let li_items = document.getElementById('lists');
-//     let lists = `<li id=${vals.name}>${vals.name} ${vals.value}
-//                 <button onClick=deleteVal('${vals.name}${vals.value}','${vals.name}')>Delete</button> 
-//                 <button onClick=editVal('${vals.name}${vals.value}','${vals.name}','${vals.value}')>Edit</button></li>`;
-//     li_items.innerHTML += lists;
-// });
-// function deleteVal(deletefunc,id)
-// {
-//     localStorage.removeItem(deletefunc)
-//     removeUser(id)
-// }
-// function editVal(editfunc, name,value)
-// {
-//     document.getElementById('name').value = name;
-//     document.getElementById('value').value = value;
-//     localStorage.removeItem(editfunc)
-//     removeUser(name)
-// }
-// function removeUser(id)
-// {
-//     const parentNode = document.getElementById('lists');
-//     const deleted = document.getElementById(id);
-//     if(deleted) {
-//         parentNode.removeChild(deleted)
-//     }
-// }
+
+window.addEventListener("DOMContentLoaded", () =>{
+    axios.get("https://crudcrud.com/api/24e5019e705344efa3e264d37ccd0792/objects")
+    .then((responses) =>
+    {
+        for(var i=0;i<responses.data.length;i++)
+        {
+            addDataInto(responses.data[i])
+        }
+        
+    }).catch((err) => console.log(err))
+})
