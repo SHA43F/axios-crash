3e2eb9c1ded86efe35fb265984f11fa356ad4a4a
SHA43F <shareefpmk44@gmail.com>
2022-10-06 12:32:30 +0530

    Deletion App_Booking Server

--- App_Booking.js
+++ App_Booking.js
@@ -8,37 +8,56 @@ edit_btn.className = 'edit'
 edit_btn.innerText = 'Edit'
 del_btn.className = 'del'
 del_btn.innerText = 'Del'
-sub.addEventListener('submit', submitNodes)
-function submitNodes(e)
-{
-    e.preventDefault();
-    let obj = {name: inpName.value, value: inpValue.value}
-    axios.post("https://crudcrud.com/api/24e5019e705344efa3e264d37ccd0792/objects", obj)
-    .then((response) => {
-        addDataInto(response)})
-    .catch((err) => console.log(err))
-    
-}
-
-function addDataInto(obj)
-{
-    let li_items = document.getElementById('lists');
-    let lists = `<li id=${obj.name}>${obj.name} ${obj.value}
-                <button onClick=deleteVal('${obj.name}${obj.value}','${obj.name}')>Delete</button> 
-                <button onClick=editVal('${obj.name}${obj.value}','${obj.name}','${obj.value}')>Edit</button></li>`;
-    li_items.innerHTML += lists;
-    inpName.value = ""
-    inpValue.value = ""
-}
 
 window.addEventListener("DOMContentLoaded", () =>{
-    axios.get("https://crudcrud.com/api/24e5019e705344efa3e264d37ccd0792/objects")
+    axios.get("https://crudcrud.com/api/53d3fcb3bbc64e6785539a8d5d020aed/objects")
     .then((responses) =>
     {
         for(var i=0;i<responses.data.length;i++)
         {
             addDataInto(responses.data[i])
         }
-        
+
     }).catch((err) => console.log(err))
 })
+
+sub.addEventListener('submit', submitNodes)
+function submitNodes(e)
+{
+    e.preventDefault();
+    let obj = {name: inpName.value, value: inpValue.value}
+    axios.post("https://crudcrud.com/api/53d3fcb3bbc64e6785539a8d5d020aed/objects", obj)
+    .then((response) => {
+        addDataInto(obj,response.data._id)})
+    .catch((err) => console.log(err))
+
+}
+
+function addDataInto(obj,id)
+{
+    let li_items = document.getElementById('lists');
+    let lists = `<li id='${id}List'>${obj.name} ${obj.value}
+                <button onClick=deleteVal('${id}')>Delete</button> 
+                <button onClick=editVal('${id}','${obj.name}','${obj.value}')>Edit</button></li>`;
+    li_items.innerHTML += lists;
+    inpName.value = ""
+    inpValue.value = ""
+}
+
+function deleteVal(id)
+{
+    axios.delete(`https://crudcrud.com/api/53d3fcb3bbc64e6785539a8d5d020aed/objects/${id}`)
+    .then((response) =>
+    {
+        removeUser(id)
+    }).catch((err) => console.log(err))
+    
+}
+function removeUser(ids)
+{
+    const parentNode = document.getElementById('lists');
+    const deleted = document.getElementById(`${ids}List`);
+    if(deleted) {
+        parentNode.removeChild(deleted)
+    }
+}
